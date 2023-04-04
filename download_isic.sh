6d6ef349b90479d93de0954bf435edc134c4c166
Han, Seunghan <hanseunghan@gmail.com>
2023-04-04 10:18:26 +0000

    add notebooks

--- download_isic.sh
+++
@@ -1,6 +0,0 @@
-wget https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Training_Data.zip
-wget https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Validation_Data.zip
-wget https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Test_v2_Data.zip
-wget https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Training_Part1_GroundTruth.zip
-wget https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Validation_Part1_GroundTruth.zip
-wget https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Test_v2_Part1_GroundTruth.zip
